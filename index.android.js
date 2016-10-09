2801e852e0788be0b8d2664c3c7af007f6e5adbf
AntonioKoteles <antonio.koteles@xoomworks.com>
2016-10-09 12:00:13 +0300

    project added to git folder

---
+++ index.android.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Awesome extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>
+          Hello Toni, nice to see you 
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: 'red',
+    marginBottom: 5
+  },
+});
+
+AppRegistry.registerComponent('Awesome', () => Awesome);
