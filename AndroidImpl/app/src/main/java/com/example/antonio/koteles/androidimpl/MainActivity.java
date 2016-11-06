7bd33509377faa36462f052ae1daee72b3060f59
AntonioKoteles <antonio.koteles@xoomworks.com>
2016-11-06 23:12:46 +0200

    Android: ListVIewItems are clickable and new activity shows

--- AndroidImpl/app/src/main/java/com/example/antonio/koteles/androidimpl/MainActivity.java
+++ AndroidImpl/app/src/main/java/com/example/antonio/koteles/androidimpl/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.antonio.koteles.androidimpl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,27 +28,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt = (EditText)findViewById(nameInput);
-        show = (ListView)findViewById(R.id.myListView);
-        save = (Button)findViewById(R.id.addBtn);
+        txt = (EditText) findViewById(nameInput);
+        show = (ListView) findViewById(R.id.myListView);
+        save = (Button) findViewById(R.id.addBtn);
+
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String getInput = txt.getText().toString();
 
-                if(addArray.contains(getInput)){
-                    Toast.makeText(getBaseContext(),"Item already there",Toast.LENGTH_LONG).show();
-                }else if(getInput == null || getInput.trim().equals("")){
-                    Toast.makeText(getBaseContext(),"IInput is empty",Toast.LENGTH_LONG).show();
-                }else{
+                if (addArray.contains(getInput)) {
+                    Toast.makeText(getBaseContext(), "Item already there", Toast.LENGTH_LONG).show();
+                } else if (getInput == null || getInput.trim().equals("")) {
+                    Toast.makeText(getBaseContext(), "IInput is empty", Toast.LENGTH_LONG).show();
+                } else {
                     addArray.add(getInput);
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, addArray);
                     show.setAdapter(adapter);
-                    ((EditText)findViewById(R.id.nameInput)).setText("");
+                    ((EditText) findViewById(R.id.nameInput)).setText("");
                 }
             }
         });
+
+
+
+
+        show.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                if (position == 0) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 1) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 2) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 3) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 4) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 5) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 6) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity1.class);
+                    startActivityForResult(myIntent, 0);
+                }
+
+                if (position == 7) {
+                    Intent myIntent = new Intent(view.getContext(), ListItemActivity2.class);
+                    startActivityForResult(myIntent, 0);
+                }
+            }
+        });
+
     }
-
-
 }
